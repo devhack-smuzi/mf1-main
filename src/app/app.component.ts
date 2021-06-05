7f17c95aac5db3ee6be6e1ffe1b2354df76938ce
d.doroshenko <d.doroshenko@mt-innovations.ru>
2021-06-05 23:52:18 +0300

    did layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'mf1-main',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mf1-main';
 }
